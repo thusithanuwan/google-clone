781671b5bc694a3164d5328c953a1f893edf0cf4
thusitha <nuwan12403482@gmail.com>
2023-07-25 17:49:17 +0530

    Added listeners to aside section

---
+++ js/main.js
@@ -0,0 +1,16 @@
+const aside = document.querySelector('aside');
+aside.addEventListener('click',(eventData)=>{
+    eventData.stopImmediatePropagation();
+
+});
+
+const apps = document.querySelector('#apps');
+apps.addEventListener('click',(eventData)=>{
+    eventData.stopImmediatePropagation();
+    aside.style.visibility = 'visible';
+
+});
+document.querySelector('html').addEventListener('click',(eventData)=>{
+    aside.style.visibility = 'hidden';
+
+});
